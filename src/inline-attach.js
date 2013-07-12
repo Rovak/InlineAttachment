07c0e323bc71e9b28eefc671c6378228090f0fce
Sasha Koss <kossnocorp@gmail.com>
2013-07-12 13:47:44 +0700

    Get rid of nasty tabs

--- src/inline-attach.js
+++ src/inline-attach.js
@@ -51,12 +51,12 @@
 
             // Attach the file. If coming from clipboard, add a default filename (only works in Chrome for now)
             // http://stackoverflow.com/questions/6664967/how-to-give-a-blob-uploaded-as-formdata-a-file-name	 
-						if (file.name) {
-							var fileNameMatches = file.name.match(/\.(.+)$/); 
-							if (fileNameMatches) {
-							  extension = fileNameMatches[1];
-							}
-						}
+            if (file.name) {
+              var fileNameMatches = file.name.match(/\.(.+)$/); 
+              if (fileNameMatches) {
+                extension = fileNameMatches[1];
+              }
+            }
             formData.append(settings.uploadFieldName, file, "image-" + Date.now() + "." + extension);
 
             xhr.open('POST', settings.uploadUrl);
