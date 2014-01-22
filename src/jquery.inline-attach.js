e9bc88bce26c4fd5db4fbc1f00242529cab38280
Tihan A. <hmftw@users.noreply.github.com>
2014-01-22 14:11:07 +0100

    Minor fix...
    Style Guide..

--- src/jquery.inline-attach.js
+++ src/jquery.inline-attach.js
@@ -11,29 +11,25 @@
     "use strict";
 
     function jQueryEditor(instance, opt) {
-
         var $this = $(instance);
         var ret = {
             getValue: function() {
                 return $this.val();
             },
             setValue: function(val) {
-                
                 // Small fix to could be able to attach the inline method to an image tag
                 switch ($this.prop('tagName').toLowerCase()){
                     case 'img':
-                      $this.attr('src', val);
-                      break;
+                    $this.attr('src', val);
+                    break;
                     default:
-                      $this.val(val);
+                    $this.val(val);
                 }
             },
             getNecessary: []
         };
-
-        if(opt.neededAttrData !== 'undefined' && opt.exFormData !== 'undefined') {
+        if(opt.neededAttrData !== undefined && opt.exFormData !== undefined') {
             $.each(opt.neededAttrData,function(i,val_){
-                
                 /* Example to get it customized to my project: The needed Infos(example: rel-Attribute) are only @the Lookcontainer...
                  ret.getNecessary[opt.exFormData[i]] = $(instance).parents('.container').attr(val_); 
                  That is the part there the options like this:*/
@@ -50,7 +46,7 @@
                 At this example the 'rel'-Attribute will set to be send as 'relatedToId' @ the formData() functionality @ L. 66 - 74 (inline-attach.js)
                 */
                 // getNecessary is the collector which will contain all options for the formData() functionality @each object
-                ret.getNecessary[opt.exFormData[i]] = $(instance).attr(val_);
+                ret.getNecessary[opt.exFormData[i]] = $(instance).parents('.overContent').attr(val_);
             });
         }
 
