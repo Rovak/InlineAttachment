af85df23c7c78dfd1bce1a212f6574d87eb5b733
Roy van Kaathoven <royvankaathoven@hotmail.com>
2014-07-13 15:42:31 +0200

    added codemirror

---
+++ src/codemirror.inline-attachment.js
@@ -0,0 +1,59 @@
+/*jslint newcap: true */
+/*global inlineAttach: false */
+/**
+ * CodeMirror version for inlineAttachment
+ *
+ * Call inlineAttach.attachToCodeMirror(editor) to attach to a codemirror instance
+ */
+(function(document, window) {
+  'use strict';
+
+  inlineAttachment.editors.codemirror = {};
+
+  var codeMirrorEditor = function(instance) {
+
+    if (!instance.getWrapperElement) {
+      throw "Invalid CodeMirror object given";
+    }
+
+    var codeMirror = instance;
+
+    return {
+      getValue: function() {
+        return codeMirror.getValue();
+      },
+      setValue: function(val) {
+        var cursor = codeMirror.getCursor();
+        codeMirror.setValue(val);
+        codeMirror.setCursor(cursor);
+      }
+    };
+  };
+
+  /**
+   * Attach InlineAttachment to CodeMirror
+   *
+   * @param {CodeMirror} codeMirror
+   */
+  inlineAttachment.editors.codemirror.attachToCodeMirror = function(codeMirror, options) {
+
+    options = options || {};
+
+    var editor = new codeMirrorEditor(codeMirror),
+      inlineattach = new inlineAttachment(options, editor),
+      el = codeMirror.getWrapperElement();
+
+    el.addEventListener('paste', function(e) {
+      inlineattach.onPaste(e);
+    }, false);
+
+    codeMirror.setOption('onDragEvent', function(data, e) {
+      if (e.type === "drop") {
+        e.stopPropagation();
+        e.preventDefault();
+        return inlineattach.onDrop(e);
+      }
+    });
+  };
+
+})(document, window);
