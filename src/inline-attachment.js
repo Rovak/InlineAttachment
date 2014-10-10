df66450c2e6d7333d99fda0a27b08c5e4d2a4d41
Roy van Kaathoven <royvankaathoven@hotmail.com>
2014-10-10 23:18:29 +0200

    start firefox prototype

--- src/inline-attachment.js
+++ src/inline-attachment.js
@@ -103,6 +103,33 @@
         el.focus();
       }
       el.scrollTop = scrollPos;
+    },
+
+    /**
+     * Converts a dataURI to a blob which can be used for FormData
+     *
+     * @see http://stackoverflow.com/a/5100158/916247
+     * @param  {String} dataURI DataUri from a file or image
+     * @return {Blob} Converted blob data
+     */
+    dataURItoBlob: function(dataURI) {
+      // convert base64/URLEncoded data component to raw binary data held in a string
+      var byteString;
+      if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+      else
+        byteString = unescape(dataURI.split(',')[1]);
+
+      // separate out the mime component
+      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+      // write the bytes of the string to a typed array
+      var ia = new Uint8Array(byteString.length);
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      return new Blob([ia], {type:mimeString});
     }
   };
 
