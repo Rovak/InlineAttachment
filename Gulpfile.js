f51a0147a6dbcb6f6b896feeb24dbf91424a8223
Roy van Kaathoven <ik@royvankaathoven.nl>
2016-10-25 19:21:56 +0200

    added watch command for building sources

--- Gulpfile.js
+++ Gulpfile.js
@@ -49,3 +49,14 @@ gulp.task("default", function(callback) {
         callback();
     });
 });
+
+gulp.task("watch", function(callback) {
+    webpack(webpackOptions({
+        watch: true,
+        debug: true
+    }), function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString());
+        callback();
+    });
+});
