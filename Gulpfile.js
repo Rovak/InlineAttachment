cc161da1aabf37b677b0da41fe0a5e5ef3d050b9
Roy van Kaathoven <ik@royvankaathoven.nl>
2016-10-25 19:22:45 +0200

    removed callback from watch command

--- Gulpfile.js
+++ Gulpfile.js
@@ -50,13 +50,12 @@ gulp.task("default", function(callback) {
     });
 });
 
-gulp.task("watch", function(callback) {
+gulp.task("watch", function() {
     webpack(webpackOptions({
         watch: true,
         debug: true
     }), function(err, stats) {
         if(err) throw new gutil.PluginError("webpack", err);
         gutil.log("[webpack]", stats.toString());
-        callback();
     });
 });
