b65217b260ffc8761eb1ecd51c8afec6ff35a348
Roy van Kaathoven <ik@royvankaathoven.nl>
2016-10-25 19:14:46 +0200

    added webpack

---
+++ Gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require("gulp");
+const gutil = require("gulp-util");
+const webpack = require("webpack");
+var path = require("path");
+
+function webpackOptions(options) {
+    return {
+        watch: options.watch,
+        entry: {
+            vanilla:    './src/inline-attachment.js',
+            angular:    './src/angularjs.inline-attachment.js',
+            input:      './src/input.inline-attachment.js',
+            jquery:     './src/jquery.inline-attachment.js'
+        },
+        devtool: options.debug ? 'inline-source-map' : '',
+        debug: options.debug,
+        output: {
+            path: __dirname + "/dist",
+            filename: "[name].js",
+            publicPath: options.publicPath
+        },
+        resolve: {
+            root: [path.join(__dirname, "packages")],
+            extensions: ['', '.js']
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['es2015']
+                    }
+                }
+            ]
+        }
+    };
+}
+
+
+gulp.task("default", function(callback) {
+    webpack(webpackOptions({
+        watch: false,
+        debug: false
+    }), function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString());
+        callback();
+    });
+});
